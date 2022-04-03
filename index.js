eee25102af478f197786766f00f86a0929cbd1ab
palanirban07 <anirbanpal1234@gmail.com>
2022-04-03 13:32:57 +0530

    #

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function changeColor(color) {
-    document.body.style.background = color;
-}
-  function Button() {
-    changeColor('aquamarine');
-}         
+       
+          function random_bg_color() {
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+            var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+         console.log(bgColor);
+          
+            document.body.style.background = bgColor;
+            }
+        
+        random_bg_color(); 
